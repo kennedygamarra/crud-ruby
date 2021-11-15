fddef1de3bce7db0dd60f23fe671f8a6d84795e8
Juan Sanchez <juan.sanchez@qrvey.com>
2021-11-14 20:10:15 -0500

    Add styles users sections

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -25,7 +25,7 @@ class UsuariosController < ApplicationController
 
     respond_to do |format|
       if @usuario.save
-        format.html { redirect_to @usuario, notice: "Usuario was successfully created." }
+        format.html { redirect_to @usuario, notice: "Usuario creado exitosamente." }
         format.json { render :show, status: :created, location: @usuario }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class UsuariosController < ApplicationController
   def update
     respond_to do |format|
       if @usuario.update(usuario_params)
-        format.html { redirect_to @usuario, notice: "Usuario was successfully updated." }
+        format.html { redirect_to @usuario, notice: "Usuario actualizado exitosamente." }
         format.json { render :show, status: :ok, location: @usuario }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -51,7 +51,7 @@ class UsuariosController < ApplicationController
   def destroy
     @usuario.destroy
     respond_to do |format|
-      format.html { redirect_to usuarios_url, notice: "Usuario was successfully destroyed." }
+      format.html { redirect_to usuarios_url, notice: "Usuario eliminado correctamente." }
       format.json { head :no_content }
     end
   end
